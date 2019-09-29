22a4711dcc7d40ae3fa22b8345de3d1f49f8e214
Jason Liu <jasonliu21396@gmail.com>
2019-09-28 19:09:37 -0700

    jj

---
+++ public/main/build/precache-manifest.9de2f59d4de8d9719f025ada0966069c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0a45e01301b0a390f3032b8be6ec91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b787da511a673bb1b01a",
+    "url": "/static/css/main.18e32cf2.chunk.css"
+  },
+  {
+    "revision": "485bde12f48549d36a54",
+    "url": "/static/js/2.3cb26851.chunk.js"
+  },
+  {
+    "revision": "b787da511a673bb1b01a",
+    "url": "/static/js/main.6a5fc1a2.chunk.js"
+  },
+  {
+    "revision": "1dc6eae1c85739e4370a",
+    "url": "/static/js/runtime~main.b4030dff.js"
+  }
+]);
